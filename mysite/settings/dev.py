feb625bec16a647f33b5f5ce2834bbd525656f3e
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2023-03-15 14:58:05 +0900

    Setup wagtail

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-m39r^*y4w@ho%y*(53##2q=9q(0so0el00z!e))=&6&)p8v!du"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
